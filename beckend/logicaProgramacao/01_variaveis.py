e5bfad332eea363ac4e1a1fd1f429e1c869920c8
Rafaelnoxi <rafatravain@gmail.com>
2025-06-17 09:44:53 -0300

    logica_de_programacao

---
+++ beckend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,17 @@
+nome = Rafael
+sobre_nome = Travain
+idade = 17
+cpf = 10832461946
+altura = 1.88
+peso = 75.5
+tem_habilitacao = False
+tem_titulodeeleitor = False
+
+print("Nome:", nome)
+print("Sobre Nome:", sobre_nome)
+print("Idade:", idade)
+print("CPF:", cpf)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Tem Habilitacao?", tem_habilitacao)
+print("Tem Titulo de eleitor?", tem_titulodeeleitor)
