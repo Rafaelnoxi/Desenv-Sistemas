c1e1501348f27a0fc0840b7502843df9f6e5e6b1
rafaelnoxi <rafatravain@gmail.com>
2025-08-19 09:32:26 -0300

    aula dia 19/08

---
+++ beckend/logicaProgramacao/05_Desafio.py
@@ -0,0 +1,9 @@
+LIsta_de_tarefas = ["lavar_roupa", "arrumar_o_quarto", "lavar_a_varanda", "lavar_a_louca", "dar_comida_cachorro"]
+print(LIsta_de_tarefas)
+LIsta_de_tarefas.insert(2, "levar_cachorro_passear")
+print(LIsta_de_tarefas)
+LIsta_de_tarefas.remove("lavar_a_louca")
+print(LIsta_de_tarefas)
+LIsta_de_tarefas.pop(1)
+print(LIsta_de_tarefas)
+
