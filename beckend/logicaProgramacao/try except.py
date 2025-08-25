e835e6978f571e7f5e125b2809796c78ac9d0b86
rafaelnoxi <rafatravain@gmail.com>
2025-08-25 08:49:42 -0300

    aula dia 25/08

---
+++ beckend/logicaProgramacao/try except.py
@@ -0,0 +1,9 @@
+#o que é o try except?
+#é uma função usada para quando acontecer algumas exceções durante o codigo e apresentar algum erro.
+
+#temos alguns blocos para o try except - try - except - else - finally
+# - try - o codigo que pode conter uma exceção
+# - except - se ocorrer uma exceção, o try procurá o bloco except para executar
+# - else - caso nao ocorra nenhuma exceção o bloco else será executado ( não é obrigatorio )
+# - finally - ele é sempre executado ( não é obrigatorio )
+
