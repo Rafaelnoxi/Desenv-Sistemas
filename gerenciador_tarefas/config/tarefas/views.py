3973b6b6048ddef694a4ed5fd3cc1ff623723636
rafaelnoxi <rafatravain@gmail.com>
2025-10-21 09:42:09 -0300

    aula dia 21/10/

---
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tarefa
+from django.http import HttpResponse
+def listar_tarefas(request):
+    tarefas_salvas=Tarefa.objects.all()
+    contexto={"minhas_tarefas":tarefas_salvas}
+    return render (request,'tarefas/lista.html',contexto)
+
+def detalhe_tarefa(request, tarefa_id):
+
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
