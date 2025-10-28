56adda1c01eb02e5338ac422fbb46758d4f5f818
rafaelnoxi <rafatravain@gmail.com>
2025-10-28 09:01:35 -0300

    aula dia 28/10

--- gerenciador_tarefas/config/tarefas/models.py
+++ gerenciador_tarefas/config/tarefas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# Create your models here.
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField (blank=True)
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Tarefa
 from django.http import HttpResponse
+
 def listar_tarefas(request):
-    tarefas_salvas=Tarefa.objects.all()
-    contexto={"minhas_tarefas":tarefas_salvas}
-    return render (request,'tarefas/lista.html',contexto)
-
+    tarefas_salvas = Tarefa.objects.all()
+    contexto = {"minhas_tarefas": tarefas_salvas}
+    return render(request, 'tarefas/lista.html', contexto)
+    
 def detalhe_tarefa(request, tarefa_id):
-
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
-
     return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+
+def adicionar_tarefa(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        Tarefa.objects.create(titulo=titulo, descricao=descricao)
+        return redirect('listar_tarefas')
+    return render(request, 'tarefas/form_tarefa.html')
+
+def alterar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404 (Tarefa, pk = tarefa_id)
+    if request.method == 'post':
+        tarefa.titulo = request.post.get('titulo')
+        tarefa.descricao = request.post.get('descricao')
+        tarefa.concluida = request.post.get('concluida') == 'on'
+        tarefa.save()
+        return redirect ('lista_tarefas')
+    return render(request,'tarefa/from_tarefa.html', {'tarefa': tarefa})
